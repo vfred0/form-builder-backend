cfb4cfd60f9d821847a0d502943f930eb7c6937f
Víctor Arreaga <arreagav17@gmail.com>
2024-09-15 13:54:43 -0500

    Refactored

--- FormBuilder.Repositories/Repository/Repository.cs
+++ FormBuilder.Repositories/Repository/Repository.cs
@@ -7,36 +7,10 @@ namespace FormBuilder.Repositories.Repository;
 public abstract class Repository<TEntity>(DbContext dbContext) : IRepository<TEntity>
     where TEntity : EntityBase
 {
-    // public async Task<ICollection<TEntity>> GetAsync()
-    // {
-    //     return await dbContext.Set<TEntity>()
-    //         .AsNoTracking()
-    //         .IgnoreQueryFilters()
-    //         .ToListAsync();
-    // }
-
     public virtual async Task<TEntity?> GetAsync(string id)
     {
         return await dbContext.Set<TEntity>()
             .FindAsync(id);
-    }
-    //
-    // public async Task<ICollection<TEntity>> GetAsync(Expression<Func<TEntity, bool>> predicate)
-    // {
-    //     return await dbContext.Set<TEntity>()
-    //         .Where(predicate)
-    //         .AsNoTracking()
-    //         .ToListAsync();
-    // }
-
-    public async Task<ICollection<TEntity>> GetAsync<TKey>(Expression<Func<TEntity, bool>> predicate,
-        Expression<Func<TEntity, TKey>> orderBy)
-    {
-        return await dbContext.Set<TEntity>()
-            .Where(predicate)
-            .OrderBy(orderBy)
-            .AsNoTracking()
-            .ToListAsync();
     }
 
     public virtual async Task<Guid> AddAsync(TEntity entity)
